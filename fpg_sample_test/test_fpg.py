3c50cf0dbf04df2198911909036c975bcdbc999c
Mohammed Shahanas <AL2516@accionlabs.in>
2020-01-12 05:21:57 +0530

    Added TestRail config file

--- fpg_sample_test/test_fpg.py
+++ fpg_sample_test/test_fpg.py
@@ -1,13 +1,16 @@
 import pytest
+from pytest_testrail.plugin import pytestrail
 
 
+@pytestrail.case('C1')
 def test_add():
     a = 5
     b = 10
     assert (a+b) == 15
 
 
+@pytestrail.case('C2')
 def test_sub():
     a = 5
     b = 10
-    assert (b-a) == 10
+    assert (b-a) == 5
