0ac31a7a3cda342ec97156848d0c56d4b2c89740
Mohammed Shahanas <AL2516@accionlabs.in>
2020-01-13 16:43:15 +0530

    Added logging module

--- fpg_sample_test/test_fpg.py
+++ fpg_sample_test/test_fpg.py
@@ -1,9 +1,15 @@
 import pytest
+import logging
 from pytest_testrail.plugin import pytestrail
 
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 @pytestrail.case('C1')
 def test_add():
+    logger.info('Logged INFO message1')
+    logger.warning('Logged WARNING message1')
+    logger.error('Logged ERROR message1')
     print("Test Add")
     a = 5
     b = 10
@@ -13,6 +19,9 @@ def test_add():
 
 @pytestrail.case('C2')
 def test_sub():
+    logger.info('Logged INFO message2')
+    logger.warning('Logged WARNING message2')
+    logger.error('Logged ERROR message2')
     print("Test Sub")
     a = 5
     b = 10
