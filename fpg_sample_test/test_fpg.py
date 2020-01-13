84c0caa5ce581daa2753f580130c23800f172910
Mohammed Shahanas <AL2516@accionlabs.in>
2020-01-13 14:38:01 +0530

    Modified tests

--- fpg_sample_test/test_fpg.py
+++ fpg_sample_test/test_fpg.py
@@ -4,13 +4,17 @@ from pytest_testrail.plugin import pytestrail
 
 @pytestrail.case('C1')
 def test_add():
+    print("Test Add")
     a = 5
     b = 10
     assert (a+b) == 15
+    print("Success - Test Add")
 
 
 @pytestrail.case('C2')
 def test_sub():
+    print("Test Sub")
     a = 5
     b = 10
     assert (b-a) == 5
+    print("Success - Test Sub")
